54a0b76a8fa0460d2b417c80473f8a0b62e70f9a
Wilfried Meindl <wilfried.meindl@gmail.com>
2015-10-08 07:27:14 +0200

    First sketch of configuration file handling

--- weather.sh
+++ weather.sh
@@ -2,20 +2,40 @@
 
 AWKLIB=/usr/local/share/awklib
 AWKFILE_DIR=/usr/local/share/weather
-
-# Trieching
-# WEATHER_LOCATION=700017
-
-# Juarez
-# WEATHER_LOCATION=116556
-
-# Playa del Carmen
-# WEATHER_LOCATION=136973
-
-# Regensburg
-WEATHER_LOCATION=${1:-687337}
+WEATHER_PLACES=~/.weather-places
 
 WEATHER_UNIT="c"
+
+while getopts f:u: opt; do
+    case $opt in
+	f) WEATHER_PLACES=$OPTARG
+	    ;;
+	u) WEATHER_UNIT=$OPTARG
+	    ;;
+    esac
+fi
+
+shift $((OPTIND - 1))
+
+# if $1; then
+    # if $1 ~ <WOEID> then
+    #	WEATHER_LOCATION=$1
+    # else if $1 ~ <place name> then
+    #   WEATHER_LOCATION=$(get WOEID from .weather-places)
+    # else error
+# else
+    # read first place from .weather_places and determine WOEID
+    # if no first place found then error
+# fi
+
+# FORMAT FOR CONFIGURATION FILE
+# unit c/f
+# place Regensburg 687337
+# place Trieching 700017
+# place Juarez 116556
+# place Playa del Carmen 136973
+
+WEATHER_LOCATION=${1:-687337}
 
 ping -q -c 1 "weather.yahooapis.com" 1>/dev/null || exit 1
 
