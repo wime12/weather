fc71c7130c22047cd203527ec240b715fe0650b1
Wilfried Meindl <wilfried.meindl@gmail.com>
2015-10-09 14:17:32 +0200

    More work on rc-file reading
    * first draft finished
    * needs more testing
    * default values (environment variables, ...) partially not repected

--- weather.sh
+++ weather.sh
@@ -5,53 +5,65 @@ AWKFILE_DIR=/usr/local/share/weather
 WEATHERRC=~/.weatherrc
 
 WEATHER_UNIT="c"
+WEATHER_WOEID=""
+
+usage() {
+    echo "$0 [-f rcfile] [-u unit] [woeid|place]"
+}
 
 while getopts f:u: opt; do
     case $opt in
-	f) WEATHERRC=$OPTARG
-	    ;;
-	u) WEATHER_UNIT=$OPTARG
-	    ;;
+        f) WEATHERRC=$OPTARG
+            ;;
+        u) WEATHER_UNIT=$OPTARG
+            ;;
     esac
-fi
+done
 
 shift $((OPTIND - 1))
 
-if $(($# == 0)); then
-    line=$(egrep -m 1 '^[[:space:]]*place[[:space:]]+.*[[:digit:]]+[[:space:]]*$' "$WEATHERRC")
-    if [ -n $line ]""; then
-	error "no default location found"
-    else
-	woeid=$(echo $line | egrep -o "[[:digit:]]+[[:space:]]*$")
-	woeid=${woeid%%[!0-9]*}
-    fi
-elsif $(($# == 1)); then
-    if $1 ~ WOEID; then
-	woeid=$1
-    else
-	get_WOEID_from_rc
-	if WOEID_found
-	   woeid=$location
-	else
-	    error "location not found"
-	    usage
-	fi
-    fi
-else
-    error "too many arguments"
-    usage
+if [ ! -f "$WEATHERRC" ]; then
+    echo "$0: configuration file $WEATHERRC not found"
+    exit 1
 fi
 
-# FORMAT FOR CONFIGURATION FILE
-# unit c/f
-# place Regensburg 687337
-# place Trieching 700017
-# place Juarez 116556
-# place Playa del Carmen 136973
+line=$(egrep -m 1 '^[[:blank:]]*unit[[:blank:]]+[cf][[:blank:]]*$'
+if [ -n "line" ]; then
+    WEATHER_UNIT=$(echo "$line" | egrep -o "[cf][[:blank:]]*$")
+    WEATHER_UNIT=${WEATHER_UNIT%%[!cf]}
+fi
 
-woeid=${1:-687337}
+# TODO: Do not read location from configuration file if WOEID is set
+case $# in
+    0)  line=$(egrep -m 1 '^[[:blank:]]*place[[:blank:]]+.*[0-9]+[[:blank:]]*$' \
+            "$WEATHERRC")
+        if [ -z "$line" ]; then
+            echo "$0: no default location found in $WEATHERRC" >&2
+	    exit 1
+        else
+            woeid=$(echo $line | egrep -o "[0-9]+[[:blank:]]*$")
+            woeid=${woeid%%[!0-9]*}
+        fi
+        ;;
+    1)  if echo $1 | egrep -q "^[0-9]+$"; then
+            woeid=$1
+        else
+            line=$(egrep "^[[:blank:]]*place[[:blank:]]+$1[[:blank:]]+[0-9]+[[:blank:]]*$" \
+                "$WEATHERRC")
+            if [ -n "$line" ]; then
+                woeid=$(echo $line | egrep -o "[0-9]+[[:blank:]]*$")
+                woeid=${woeid%%[!0-9]*}
+            else
+                echo "$0: location \"$1\" not found" >&2
+		exit 1
+            fi
+        fi
+        ;;
+    *)  echo "$0: too many arguments" >&2
+        usage
+	exit 1
+esac
 
-ping -q -c 1 "weather.yahooapis.com" 1>/dev/null || exit 1
-
-fetch -q -o - "http://weather.yahooapis.com/forecastrss?w=${woeid}&u=${WEATHER_UNIT}" | \
+# fetch -q -o -
+curl -m 4 -s "http://weather.yahooapis.com/forecastrss?w=${woeid}&u=${WEATHER_UNIT}" | \
 awk -f $AWKLIB/getxml.awk -f $AWKFILE_DIR/weather.awk /dev/stdin
