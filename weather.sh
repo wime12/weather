f2189c03879e1aae4f4bdc7227f50ac67818c245
Wilfried Meindl <wilfried.meindl@gmail.com>
2015-10-09 07:36:17 +0200

    Start work on configuration file
    * case of no argument: read default place

--- weather.sh
+++ weather.sh
@@ -2,13 +2,13 @@
 
 AWKLIB=/usr/local/share/awklib
 AWKFILE_DIR=/usr/local/share/weather
-WEATHER_PLACES=~/.weather-places
+WEATHERRC=~/.weatherrc
 
 WEATHER_UNIT="c"
 
 while getopts f:u: opt; do
     case $opt in
-	f) WEATHER_PLACES=$OPTARG
+	f) WEATHERRC=$OPTARG
 	    ;;
 	u) WEATHER_UNIT=$OPTARG
 	    ;;
@@ -17,16 +17,30 @@ fi
 
 shift $((OPTIND - 1))
 
-# if $1; then
-    # if $1 ~ <WOEID> then
-    #	WEATHER_LOCATION=$1
-    # else if $1 ~ <place name> then
-    #   WEATHER_LOCATION=$(get WOEID from .weather-places)
-    # else error
-# else
-    # read first place from .weather_places and determine WOEID
-    # if no first place found then error
-# fi
+if $(($# == 0)); then
+    line=$(egrep -m 1 '^[[:space:]]*place[[:space:]]+.*[[:digit:]]+[[:space:]]*$' "$WEATHERRC")
+    if [ -n $line ]""; then
+	error "no default location found"
+    else
+	woeid=$(echo $line | egrep -o "[[:digit:]]+[[:space:]]*$")
+	woeid=${woeid%%[!0-9]*}
+    fi
+elsif $(($# == 1)); then
+    if $1 ~ WOEID; then
+	woeid=$1
+    else
+	get_WOEID_from_rc
+	if WOEID_found
+	   woeid=$location
+	else
+	    error "location not found"
+	    usage
+	fi
+    fi
+else
+    error "too many arguments"
+    usage
+fi
 
 # FORMAT FOR CONFIGURATION FILE
 # unit c/f
@@ -35,9 +49,9 @@ shift $((OPTIND - 1))
 # place Juarez 116556
 # place Playa del Carmen 136973
 
-WEATHER_LOCATION=${1:-687337}
+woeid=${1:-687337}
 
 ping -q -c 1 "weather.yahooapis.com" 1>/dev/null || exit 1
 
-fetch -q -o - "http://weather.yahooapis.com/forecastrss?w=${WEATHER_LOCATION}&u=${WEATHER_UNIT}" | \
+fetch -q -o - "http://weather.yahooapis.com/forecastrss?w=${woeid}&u=${WEATHER_UNIT}" | \
 awk -f $AWKLIB/getxml.awk -f $AWKFILE_DIR/weather.awk /dev/stdin
